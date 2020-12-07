b67391176c91221814574aa3e5427fe5f9c733b3
George Matos <georgemattos@gmail.com>
2020-12-07 17:53:10 -0300

    implementacao do profile.component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { MainComponent } from './main/main.component';
+import { ProfileComponent } from './profile/profile.component';
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   { path: "", pathMatch: 'full', redirectTo: 'main' },
   { path: "login", component: LoginComponent },
   { path: "register", component: RegisterComponent },
+  { path: "profile", component: ProfileComponent },
   { path: "main", component: MainComponent }
 ];
 
